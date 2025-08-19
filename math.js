f38bc3589859ed73c7b8ac11e66cd6c695526e3c
leandroitec <l.odetto@itecriocuarto.org.ar>
2025-08-18 22:33:09 -0300

    Create math.js

---
+++ math.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 5;
+
+// Suma
+let suma = a + b;
+console.log("Suma:", suma); // 15
+
+// Resta
+let resta = a - b;
+console.log("Resta:", resta); // 5
+
+// Multiplicación
+let multiplicacion = a * b;
+console.log("Multiplicación:", multiplicacion); // 50
